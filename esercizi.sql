69cc65f70e6c6f8771b91139a8c81aa4e0153b91
HYDR00GEN <ovy.hydrogen@gmail.com>
2022-11-16 11:32:34 +0100

    es2

--- esercizi.sql
+++ esercizi.sql
@@ -13,8 +13,13 @@ tutti	i	continenti	in	cui	ha	effettuato	una	scalata,
 ordinando	il	risultato	per	codice	fiscale	e,	a	parità	di	
 codice	fiscale,	per	il	nome	del	continente  **/
 
-
-
+select scalata.scalatore, count(nazione.continente), scalatore.annonascita
+from scalata
+    join nazione on scalata.nazione = nazione.nome
+    join scalatore on scalatore.cf = scalata.scalatore
+where scalatore.annonascita < 1980
+group by scalata.scalatore, scalatore.annonascita
+order by scalata.scalatore
 
 /**  3 - Calcolare	le	nazioni	(mostrando,	per	ciascuna,	anche	il	
 continente)	nelle	quali	è	stata	effettuata	almeno	una	scalata	
